8601defae9ec83d5bea8d80cefedaec9861c1d33
Cameron Badman <cbadwork@gmail.com>
2026-04-08 05:30:33 +1000

    Fix ranged formula export

--- src/hippo_encoder/__init__.py
+++ src/hippo_encoder/__init__.py
@@ -1,11 +1,11 @@
 """Hippo encoder distillation package."""
 
 from hippo_encoder.region import RangeOp, SparseRegionProgram, inside_fraction, soft_box_distance
-from hippo_encoder.formula_region import FormulaRegionProgram, FormulaTerm
+from hippo_encoder.formula_region import FormulaRegionProgram, RangedFormulaTerm
 
 __all__ = [
     "FormulaRegionProgram",
-    "FormulaTerm",
+    "RangedFormulaTerm",
     "RangeOp",
     "SparseRegionProgram",
     "inside_fraction",
